60e3ba50426a7b00885a17b84822d425ac468e8d
Kageyama Tobio <kageyamatobio@Kageyamas-MacBook-Air.local>
2021-10-26 17:15:10 -0400

    free response and y/n, korean and jap

--- public/app.js
+++ public/app.js
@@ -1,9 +1,17 @@
-// Add a new document in collection "cities"
 const db = firebase.firestore()
 const questionContainer = document.getElementById('question-container')
 const optionsContainer = document.getElementById('options-container')
+const langDropdown = document.getElementById('lang')
+//document.getElementById('input-file').addEventListener('change', getFile)
 var answer = null
-
+var questionType = 'freeResponse'
+var lang = 'japanese'
+function updateLang(){
+    lang = langDropdown.value
+}
+function radioClick(rad){
+    questionType = rad.value
+}
 function removeAllChildNodes(parent) {
     while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
@@ -11,48 +19,55 @@ function removeAllChildNodes(parent) {
 }
 
 function populate(){
-    removeAllChildNodes(questionContainer)
-    removeAllChildNodes(optionsContainer)
-    let index = Math.floor(Math.random() * 70)
-    getQuestion(index).then(q => {
-    console.log(q.question)
-    answer = q.answer
-    let question = document.createElement('P')
-    question.innerHTML = q.question
-    questionContainer.appendChild(question)
-
-    let yesBtn = document.createElement('BUTTON')
-    yesBtn.innerHTML = 'はい'
-    yesBtn.addEventListener('click', (e, a = 1) => {
-        console.log(a == answer)
-    })
-    let noBtn = document.createElement('BUTTON')
-    noBtn.innerHTML = 'いいえ'
-    noBtn.addEventListener('click', (e, a = 0) => {
-        console.log(a == answer)
-    })
-    optionsContainer.appendChild(yesBtn)    
-    optionsContainer.appendChild(noBtn)    
-})
+    if (questionType != 'multipleChoice'){
+        removeAllChildNodes(questionContainer)
+        removeAllChildNodes(optionsContainer)
+        getQuestion().then(q => {
+            console.log(q.question)
+            answer = q.answer
+            let question = document.createElement('P')
+            question.innerHTML = q.question
+            questionContainer.appendChild(question)
+        })
+    }
+    if (questionType == 'YN'){
+        let yesBtn = document.createElement('BUTTON')
+        yesBtn.innerHTML = 'はい'
+        yesBtn.addEventListener('click', (e, a = 1) => {
+            console.log(a == answer)
+        })
+        let noBtn = document.createElement('BUTTON')
+        noBtn.innerHTML = 'いいえ'
+        noBtn.addEventListener('click', (e, a = 0) => {
+            console.log(a == answer)
+        })
+        optionsContainer.appendChild(yesBtn)    
+        optionsContainer.appendChild(noBtn)    
+    } else {
+        //add a button that shows the answer
+    }
 }
 
 
-function getQuestion(index){
-var docRef = db.collection("questions").doc("" + index);
-var getOptions = {
-    source: 'default'
-};
+function getQuestion(){
+    if (questionType != 'multipleChoice'){
 
-let result = docRef.get(getOptions).then((doc) => {
-    console.log("Document data:", doc.data());
-    return doc.data()
-}).catch((error) => {
-    console.log("Error getting document:", error);
-});
-return result
+        var docRef = db.collection("questions").doc(lang).collection(questionType).doc('' + Math.floor(Math.random()*50))
+    } else {
+        //multiple choice    
+    }
+    var getOptions = {
+        source: 'default'
+    }
+
+    let result = docRef.get(getOptions).then((doc) => {
+        console.log("Document data:", doc.data());
+        return doc.data()
+    }).catch((error) => {
+        console.log("Error getting document:", error);
+    });
+    return result
 }
-
-populate()
 /*
 function getFile(event) {
 	const input = event.target
@@ -64,11 +79,20 @@ function getFile(event) {
 
 function placeFileContent(file) {
 	readFileContent(file).then(content => {
-      questions = (content.split(']')) 
-      for (let i = 0; i < questions.length; i++){
-        db.collection("questions").doc(('' + i)).set({
-        question: questions[i].substring(3, questions[i].indexOf(',') - 1),
-        answer: questions[i].includes('YES') ? 1 : 0
+      questions = (content.split(';')).slice(0,-1)
+      free_response = []
+      yes_no = []
+      multiple_choice = []
+      
+     //for (let i = 0; i < questions.length; i+=3){
+        multiple_choice.push(questions[i])
+        multiple_choice.push(questions[i+2])
+        }
+
+      for (let i = 0; i < 200; i+=2){
+        db.collection("questions").doc('korean').collection('vocab').doc('' + i / 2).set({
+        word: multiple_choice[i],
+        meaning: multiple_choice[i+1]
         })
         .then(() => {
             console.log("Document successfully written!");
@@ -78,6 +102,41 @@ function placeFileContent(file) {
         });
       }
       
+      for (let i = 0; i < questions.length; i+=3){
+        if (questions[i+2].includes('free')){
+            free_response.push(questions[i])
+            free_response.push(questions[i+1])
+        } else {
+            yes_no.push(questions[i])
+            yes_no.push(questions[i+1])
+        }
+      }
+      for (let i = 0; i < free_response.length; i+=2){
+        db.collection("questions").doc('korean').collection('freeResponse').doc('' + i / 2).set({
+        question: free_response[i],
+        answer: free_response[i+1]
+        })
+        .then(() => {
+            console.log("Document successfully written!");
+        })
+        .catch((error) => {
+            console.error("Error writing document: ", error);
+        });
+      }
+      
+      for (let i = 0; i < yes_no.length; i+=2){
+        db.collection("questions").doc('korean').collection('YN').doc('' + i / 2).set({
+        question: yes_no[i],
+        answer: yes_no[i+1].includes('YES') ? 1 : 0
+        })
+        .then(() => {
+            console.log("Document successfully written!");
+        })
+        .catch((error) => {
+            console.error("Error writing document: ", error);
+        })
+        }
+    
   }).catch(error => console.log(error))
   
 }
